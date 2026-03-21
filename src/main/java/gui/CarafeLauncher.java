f4bc24366f1de25dd432ba5324d88eecbbe9283c
Bo Wen <wenbostar@gmail.com>
2026-03-21 14:33:42 -0700

    Remove help flag handling and usage printing from CarafeLauncher

--- src/main/java/gui/CarafeLauncher.java
+++ src/main/java/gui/CarafeLauncher.java
@@ -33,8 +33,6 @@ public class CarafeLauncher {
         // Check for GUI flag or no arguments
         if (args.length == 0 || containsGuiFlag(args)) {
             launchGUI(filterGuiFlags(args));
-        } else if (containsHelpFlag(args) && args.length == 1) {
-            printUsage();
         } else {
             // Launch CLI mode
             launchCLI(args);
@@ -44,15 +42,6 @@ public class CarafeLauncher {
     private static boolean containsGuiFlag(String[] args) {
         for (String arg : args) {
             if (arg.equalsIgnoreCase("--gui") || arg.equalsIgnoreCase("-gui")) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private static boolean containsHelpFlag(String[] args) {
-        for (String arg : args) {
-            if (arg.equalsIgnoreCase("-h") || arg.equalsIgnoreCase("--help") || arg.equalsIgnoreCase("-help")) {
                 return true;
             }
         }
@@ -82,24 +71,4 @@ public class CarafeLauncher {
         }
     }
 
-    private static void printUsage() {
-        System.out.println(BANNER);
-        System.out.println("""
-            Usage: java -jar carafe.jar [options]
-            
-            Launch Modes:
-              --gui, -gui          Launch the graphical user interface
-              (no arguments)       Launch the graphical user interface
-              (with CLI options)   Run in command line mode
-            
-            For CLI options, use: java -jar carafe.jar -h
-            
-            Examples:
-              java -jar carafe.jar                    # Launch GUI
-              java -jar carafe.jar --gui              # Launch GUI
-              java -jar carafe.jar -db protein.fasta  # Run CLI mode
-            
-            For more information, visit: https://github.com/Noble-Lab/Carafe
-            """);
-    }
 }
